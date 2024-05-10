d52e3616ff053af8ae539d65a5b9726e4e3503e2
papilon10 <140213599+papilon10@users.noreply.github.com>
2024-05-10 13:53:58 -0600

    primmer commit, clases vacias

---
+++ src/main/java/www/laboratorio4p2_salvadormacias/Laboratorio4p2_salvadormacias.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package www.laboratorio4p2_salvadormacias;
+
+/**
+ *
+ * @author Apple
+ */
+public class Laboratorio4p2_salvadormacias {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
