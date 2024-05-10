6a4e95b02238c27b3fed35d6be7f75267f4dcd80
papilon10 <140213599+papilon10@users.noreply.github.com>
2024-05-10 14:20:07 -0600

    commit obligatorio

--- src/main/java/www/laboratorio4p2_salvadormacias/Laboratorio4p2_salvadormacias.java
+++ src/main/java/www/laboratorio4p2_salvadormacias/Laboratorio4p2_salvadormacias.java
@@ -1,8 +1,10 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package www.laboratorio4p2_salvadormacias;
+
+import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  *
@@ -10,7 +12,37 @@ package www.laboratorio4p2_salvadormacias;
  */
 public class Laboratorio4p2_salvadormacias {
 
+    public static ArrayList lista = new ArrayList();
+    public static ArrayList<usuario> lista_usuarios = new ArrayList<>();
+    public static Scanner lea = new Scanner(System.in);
+    public static Scanner str = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        boolean salida = false;
+        lista.add(new admin(10, "pepe", "hernandez", "pepito123", "PEPE10"));
+        lista.add(new gerente("administracion", "oscar", "sanchez", "oscar_san", "molly77"));
+        lista.add(new empleado(0.0, "fredy", "matamoros", "darkFredy", "fredy2000"));
+        
+        do {
+            System.out.println("---Bienvenido---");
+            System.out.println("1. iniciar sesion");
+            System.out.println("2. salir");
+            int opcion = lea.nextInt();
+            if (opcion == 1) {
+                System.out.println("---Inicio de sesion---");
+                System.out.println("Ingrese el nombre del usuario: ");
+                String user= str.nextLine();
+                String
+                
+
+            } else if (opcion == 2) {
+
+            } else {
+
+                System.out.println("opcion ingresada es invalida...");
+            }
+
+        } while (salida != true);
+
     }
 }
